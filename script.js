3d74c5248075619619c8c4b6c35d456b02d03184
aaron wertheim <aawertheim@gmail.com>
2022-04-21 17:26:05 -0500

    background expand/retun tab

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const h4 = document.createElement("h4");
 h4.textContent = "Actor";
 h4.classList = h4;
 document.querySelector("body").appendChild(h4);
+document.querySelector("div").appendChild(h4);
