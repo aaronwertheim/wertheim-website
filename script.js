82497f22a9541a9c372d8816f3bc33a86bfcceac
aaron wertheim <aawertheim@gmail.com>
2022-05-13 14:26:39 -0500

    menu event listeners, css touchups

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const h4 = document.createElement("h4");
 h4.textContent = "Actor";
-h4.classList = h4;
-document.querySelector("body").appendChild(h4);
-document.querySelector("div").appendChild(h4);
+document.getElementById("name").appendChild(h4);
+
+const about = document.getElementById("about");
+const credits = document.getElementById("credits");
+
+function reduceMenu() {
+    this.textContent = "";
+}
+
+credits.addEventListener('mouseenter', function() {
+    credits.textContent = "Credits";
+})
+about.addEventListener('mouseenter', function() {
+    about.textContent = "About Me";
+})
+
+about.addEventListener('mouseleave', reduceMenu);
+credits.addEventListener('mouseleave', reduceMenu);
